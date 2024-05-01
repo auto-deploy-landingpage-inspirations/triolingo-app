456558e97334f9271df38c1bd407ff25d6ba9b4d
Daniel Lechner <daniel@lechner.io>
2024-05-01 18:11:28 +0200

    dynamic header for feed /learn

--- src/components/UserProgress.tsx
+++ src/components/UserProgress.tsx
@@ -2,12 +2,10 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 import Image from "next/image"
 import { InfinityIcon } from "lucide-react"
+import { courses } from "@/database/schema"
 
 type Props = {
-  activeCourse: {
-    title: string
-    imageSrc: string
-  } // TODO: Define type
+  activeCourse: typeof courses.$inferSelect
   hearts: number
   points: number
   hasActiveSubscription: boolean
